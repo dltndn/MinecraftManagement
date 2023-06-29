a568624f7cbb019040a2f2914b2056f703bc0901
dltndn <james98099@gmail.com>
2023-06-29 13:13:49 +0900

    fix: 오류 코드 수정

--- pages/api/setUsing.ts
+++ pages/api/setUsing.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { signUp_db } from "@/etc/executeDB";
+import { setUsing_db } from "@/etc/executeDB";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method === "POST") {
       const submitData = req.body; // POST 요청의 데이터
-      const result = await signUp_db(submitData);
+      const result = await setUsing_db(submitData);
       res.status(200).json({ result });
     } else {
       res.status(405).json({ error: "Method Not Allowed" });
